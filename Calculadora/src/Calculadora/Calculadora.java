55c74952a417b872e14ae1c0725fc6423047401e
sheila89 <sheila_bravo@hotmail.com>
2016-04-04 13:23:09 +0200

    Creación clase Calculadora

---
+++ Calculadora/src/Calculadora/Calculadora.java
@@ -0,0 +1,15 @@
+package Calculadora;
+
+public class Calculadora {
+	
+	private int numero1;
+	private int numero2;
+	
+	// constructores
+	
+	public calculadora(int numero1,int numero2){
+		this.numero1=numero1;
+		this.numero2=numero2;
+	} 
+
+}
